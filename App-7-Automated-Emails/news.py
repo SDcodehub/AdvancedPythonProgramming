317ef7823dd64f12125da76c91388bd150e3d7e5
NamrataMtawadkar”git config --global user.name NamrataMatawadkar” <namrata.sm712@gmail.com>
2023-01-12 14:48:20 +0530

    app-7 automated email sender

---
+++ App-7-Automated-Emails/news.py
@@ -0,0 +1,46 @@
+# API key f29281719e984c1193512fd4f17e309e
+import requests
+
+class NewsFeed:
+    """Representing multiple news titles and links as a single string
+    """
+    base_url = "https://newsapi.org/v2/everything?"
+    api_key = "f29281719e984c1193512fd4f17e309e"
+
+    def __init__(self, interest, from_date, to_date, language):
+        self.interest = interest
+        self.from_date = from_date
+        self.to_date = to_date
+        self.language = language
+
+    def get(self):
+        url = self._build_url()
+
+        articles = self._get_articles(url)
+
+        email_body = ''
+        for article in articles:
+            email_body = email_body + article['title'] + '\n' + article['url'] + '\n\n'
+
+        return(email_body)
+
+    def _get_articles(self, url):
+        response = requests.get(url)
+        content = response.json()
+        articles = content['articles']
+        return articles
+
+    def _build_url(self):
+        url = f"{self.base_url}" \
+              f"qInTitle={self.interest}&" \
+              f"from={self.from_date}&" \
+              f"to={self.to_date}&" \
+              f"language={self.language}&" \
+              "sortBy=popularity&" \
+              f"apiKey={self.api_key}"
+        return url
+
+
+if __name__ == "__main__":
+    news_feed = NewsFeed('nasa', '2023-01-01', "2023-01-11",'en')
+    print(news_feed.get())
